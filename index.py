4772ff9b9f41974aa360c7640231ed8d7a25036d
Jethrosky <loodabeats@gmail.com>
2025-02-12 12:42:05 +0200

    CALCULATOR

---
+++ index.py
@@ -0,0 +1,23 @@
+# My Calculator
+
+
+operand = input("Enter Your Operand * / + -")
+num1 = float(input("Enter Your First Number: "))
+num2 = float(input("Enter Your Second Number: "))
+
+if operand == "*" :
+    result = num1 * num2
+    print("The Answer Is:", result)
+
+
+if operand == "/" :
+    result = num1 / num2
+    print("The Answer Is:", result)
+
+if operand == "-" :
+    result = num1 - num2
+    print("The Answer Is:", result)
+
+if operand == "+" :
+    result = num1 + num2
+    print("The Answer Is:", result)
